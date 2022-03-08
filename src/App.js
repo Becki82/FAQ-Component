85aa3336b2ffb157e58670871806eec6161ec8f9
Rebecca Waterson <rebeccawaterson@Rebeccas-MacBook-Pro.local>
2022-03-08 20:09:26 +0000

    changes to styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header/>
         <Accordion />
-        
-        
-    
     </div>
   );
 }
