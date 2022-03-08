b6289eced7a6f01d734af0228b4c5d7541c314c1
Rebecca Waterson <rebeccawaterson@Rebeccas-MacBook-Pro.local>
2022-03-08 19:32:36 +0000

    initial changes

---
+++ src/components/Accordian.js
@@ -0,0 +1,24 @@
+import { faqs } from "../data";
+import AccordionItem from "./AccordianItem";
+import { useState } from "react";
+
+const Accordion = () => {
+    const [clicked, setClicked] = useState("0");
+
+    const handleToggle = (index) => {
+      if(clicked === index) {
+        return setClicked("0")
+      }
+      setClicked(index)
+    };
+  return (
+    <ul className="accordion">
+      {faqs.map((faq, index) => (
+        <AccordionItem onToggle={() => handleToggle(index)}
+        active={clicked === index} key={index} faq={faq} />
+      ))}
+    </ul>
+  );
+};
+
+export default Accordion;
